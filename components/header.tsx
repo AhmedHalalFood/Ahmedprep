4ebb69864675527482fbea6fce0fac39e95696dc
AhmedHalalFood <tariq@ahmedhalalfood.com>
2026-04-15 13:59:05 -0400

    Update header.tsx
    added a space on line 3

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
 
+
 export function Header() {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 border-b border-border bg-background/95 backdrop-blur">
